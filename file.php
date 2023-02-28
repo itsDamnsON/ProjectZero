89fa2e7a6e4ec13ee8cfd31f6e983c69e33017f5
itsDamnsON <93309766+itsDamnsON@users.noreply.github.com>
2023-02-28 12:53:33 +0700

    initial realease

--- file.php
+++ file.php
@@ -1,1 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "projectzero";
+
+// create connection
+$conn = mysqli_connect($servername, $username, $password, $projectzero);
+
+// check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// select data from table
+$sql = "SELECT * FROM my_table";
+$result = mysqli_query($conn, $sql);
+
+// display data in table
+echo "<table>";
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td>" . $row["user"] . "</td>";
+    echo "<td>" . $row["password"] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+// close connection
+mysqli_close($conn);
+?>
 
